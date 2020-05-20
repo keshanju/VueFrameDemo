1c74bcb3f7ae11b930f6c37b5b47dee79a7906b2
Wings/柯善炬 <wings.ke@newtype.io>
2020-05-20 18:12:39 +0800

    update首页

--- src/pages/ClientPortal/index.ts
+++ src/pages/ClientPortal/index.ts
@@ -1,10 +1,17 @@
 import "@/assets/less/main.less"
 import {Component, Vue} from "vue-property-decorator"
+import { Link, Row, Col } from "element-ui"
 import "babel-polyfill"
 
 Vue.config.productionTip = false;
 
-@Component({})
+@Component({
+    components: {
+        "el-link": Link,
+        "el-row": Row,
+        "el-col": Col
+    }
+})
 class Index extends Vue {
 }
 
